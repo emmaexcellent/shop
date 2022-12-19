9bca9fec98a0fa7e51bd83ac3093bac2a74c8677
Daddypastorgit <emmaexcellent030@gmail.com>
2022-12-19 21:53:54 +0100

    Edit Settings

--- shop/settings.py
+++ shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['shop-production-8a81.up.railway.app', '127.0.0.1']
+ALLOWED_HOSTS = ['shop-production-cfdd.up.railway.app', '127.0.0.1']
 
 
 # Application definition
@@ -167,7 +167,7 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 #DEPLOYMENT 
 
-CSRF_TRUSTED_ORIGINS = ['https://shop-production-8a81.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://shop-production-cfdd.up.railway.app']
 
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
