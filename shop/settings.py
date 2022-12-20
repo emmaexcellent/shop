c8f29411ca932f9ba773fff9d6ef833d3eb44af3
Daddypastorgit <emmaexcellent030@gmail.com>
2022-12-20 17:39:44 +0100

    Edit Settings

--- shop/settings.py
+++ shop/settings.py
@@ -95,8 +95,9 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+
     }
 }
 
