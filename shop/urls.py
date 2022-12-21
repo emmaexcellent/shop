4f59036b8b36cb59292998a178cabda834367228
Daddypastorgit <emmaexcellent030@gmail.com>
2022-12-21 15:09:32 +0100

    Edit Settings

--- shop/urls.py
+++ shop/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/admin/admin/030/', admin.site.urls),
     path('', include('main.urls')),
     path('shop/', include('product.urls')),
     path('', include('cart.urls')),
