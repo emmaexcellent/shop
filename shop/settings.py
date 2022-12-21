47fa685f38616e3ee12996730b18cfc8cc71d14a
Daddypastorgit <emmaexcellent030@gmail.com>
2022-12-21 15:21:16 +0100

    Edit Settings

--- shop/settings.py
+++ shop/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'main',
     'product',
     'cart',
