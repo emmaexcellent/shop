ab7376b27aaabee006403ca91ef5076ba45ef5e5
Daddypastorgit <emmaexcellent030@gmail.com>
2022-12-21 10:53:50 +0100

    Edit Settings

--- shop/settings.py
+++ shop/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://excelcart-production.up.railway.app']
+ALLOWED_HOSTS = ['excelcart-production.up.railway.app']
 
 
 # Application definition
