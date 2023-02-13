28897e6249e7fda910cb45d5861c69dea1977feb
Daddypastorgit <emmaexcellent030@gmail.com>
2023-02-13 22:54:03 +0100

    Edit Settings

--- shop/settings.py
+++ shop/settings.py
@@ -153,7 +153,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = os.path.join(BASE_DIR, "django-store-372412-a6affc2d3975.json")
+GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = os.path.join(BASE_DIR, "filestore-377720-b054710af7f1.json")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- static/assets/js/table-column-remove.js
+++
@@ -1,4 +0,0 @@
-$(".compare-table").on("click", ".remove_column", function (event) {
-    var ndx = $(this).parent().index() + 1;
-    $("td , th", event.delegateTarget).remove(":nth-child(" + ndx + ")");
-});
