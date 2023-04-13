c9838e2f2d32b1e3fa72370512477428a806f7fb
Daddypastorgit <emmaexcellent030@gmail.com>
2023-04-13 10:07:53 +0100

    Edit Settings

--- cart/context_processors.py
+++ cart/context_processors.py
@@ -7,7 +7,7 @@ def cart(request):
     if request.session:
         if 'cartdata' in request.session:
             for p_id,item in request.session['cartdata'].items():
-                total_amt+=int(item['qty'])*int(item['price'])
+                total_amt+=int(item['qty'])*float(item['price'])
             return {'cart_data':request.session['cartdata'],'totalitems':len(request.session['cartdata']),'total_amt':total_amt}
         else:
             return {'cart_data':'','totalitems':0,'total_amt':total_amt}  
